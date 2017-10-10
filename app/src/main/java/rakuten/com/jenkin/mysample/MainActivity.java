ecf9cd52bd998ea43831ba5fb2b62cec2c3b819a
Gajavalli, Venkata <venkata.sg@rakuten.com>
2017-10-10 09:10:58 +0530

    Initial

---
+++ app/src/main/java/rakuten/com/jenkin/mysample/MainActivity.java
@@ -0,0 +1,13 @@
+package rakuten.com.jenkin.mysample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
